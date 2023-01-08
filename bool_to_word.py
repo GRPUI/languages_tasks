837fe887c171376d70707eebcea0ac8ea8674827
GRPUI <57088395+GRPUI@users.noreply.github.com>
2023-01-09 00:23:26 +0700

    Create bool_to_word.py

---
+++ bool_to_word.py
@@ -0,0 +1,17 @@
+"""
+Завершите метод, который принимает логическое значение и возвращает строку "Да" для true или строку "Нет" для false.
+"""
+
+def bool_to_word_simple(boolean):
+  if boolean:
+    return "Yes"
+  return "No"
+
+def bool_to_word_advanced(boolean):
+    return "Yes" if boolean else "No"
+  
+  
+print(bool_to_word_simple(True))
+#ожидаемый вывод: "Yes"
+print(bool_to_word_advanced(False))
+#ожидаемый вывод: "No"
