99381fce2ff5ed3560290d19c6fcdeca286b7098
GRPUI <57088395+GRPUI@users.noreply.github.com>
2023-03-01 23:10:52 +0700

    Create bool_to_word.cs

---
+++ c#/bool_to_word.cs
@@ -0,0 +1,11 @@
+/*
+Завершите метод, который принимает логическое значение и возвращает строку "Yes" для true или строку "No" для false.
+*/
+
+public static class Kata
+{
+  public static string boolToWord(bool word)
+  {
+    return word ? "Yes" : "No";
+  }
+}
