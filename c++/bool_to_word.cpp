a7c830242d34525a6c867447d1df8c1ddddc36ed
GRPUI <57088395+GRPUI@users.noreply.github.com>
2023-03-01 23:09:46 +0700

    Create bool_to_word.cpp

---
+++ c++/bool_to_word.cpp
@@ -0,0 +1,10 @@
+/*
+Завершите метод, который принимает логическое значение и возвращает строку "Yes" для true или строку "No" для false.
+*/
+
+#include <string>
+
+std::string bool_to_word(bool word)
+{
+  return word ? "Yes" : "No";
+}
